5a640d36b92707ece422d10619b58f12f51cfde6
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 18:08:37 +0530

    Modified User

--- website/models.py
+++ website/models.py
@@ -4,6 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin):
+    def __init__(self, email= None):
+        
+        self.email = email
+    
+
     def create_user(self, username, email, password):
         hashed_password = generate_password_hash(password)
         db.users.insert_one({'username': username, 'email': email, 'password': hashed_password})
@@ -11,5 +16,9 @@ class User(UserMixin):
     def find_user(self, email):
         return db.users.find_one({'email': email})
 
-    def check_password(self, stored_password, password):
-        return check_password_hash(stored_password, password)
+    def check_password(self, password):
+        user = db.users.find_one({"email": self.email})
+        return check_password_hash(user['password'], password)
+    
+    def get_id(self):
+        return self.email
