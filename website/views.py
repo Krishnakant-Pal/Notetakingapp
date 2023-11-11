591bd1bd8fd82ba1cba90b2ad1a0f4da0db1677d
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 15:51:06 +0530

    Added signup

--- website/views.py
+++ website/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+test = {"email" : 'Krishna@gmail.com'}
+
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
+    
     return render_template('home.html')
