82ec8c8c71f7e61336db4aa2f43518449c7727ab
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 18:07:16 +0530

    Added loginmanager

--- website/__init__.py
+++ website/__init__.py
@@ -1,21 +1,35 @@
 from flask import Flask
-
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'something'
     
-
-
     
+
 
     from .views import views
     from .auth import auth
-
+    from .models import User
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     from .config import db
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        user_data = db.users.find_one({'email': user_id})
+        if user_data:
+            return User(user_data)
+        return None
+
+
+
+
     return app
 
