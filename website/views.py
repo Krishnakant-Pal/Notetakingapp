4f4522ab04d9a1dccb145ef08136adc4979bbee8
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 18:08:19 +0530

    Added login required

--- website/views.py
+++ website/views.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import login_required,current_user
 
-test = {"email" : 'Krishna@gmail.com'}
+# test = {"email" : 'Krishna@gmail.com'}
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    
     return render_template('home.html')
