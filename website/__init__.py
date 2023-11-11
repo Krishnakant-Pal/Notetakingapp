50de22ea5de22ea79e3f7704a754755c9901640e
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 15:51:32 +0530

    Updated

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,21 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'something'
+    
+
+
+    
 
     from .views import views
     from .auth import auth
 
+    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-
-
-
+    from .config import db
     return app
+
