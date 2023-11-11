46b314266011dbc10f15a7c5217cd71f9bcae823
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 18:07:38 +0530

    Added Login required

--- website/auth.py
+++ website/auth.py
@@ -1,27 +1,31 @@
 from flask import Blueprint, render_template,request,flash,redirect,url_for
+from flask_login import login_required,login_user, logout_user,current_user
 from website.models import User
 from website.config import db
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login',methods=['GET', 'POST'])
-def home():
+def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User()
+        user = User(email)
         existing_user = user.find_user(email)
 
         if not existing_user:
+        # if not user.find_user(email):
             flash('Please Signup first ', category='error')
             return redirect(url_for('auth.signup'))
 
-        elif user.check_password(existing_user['password'], password):
+        elif user.check_password( password):
+            
             flash('You are logged in', category='success')
+            login_user(user,remember=True)
             return redirect(url_for('views.home'))
         else: 
             flash('Incorrect password', category='error')
-            return redirect(url_for('auth.home'))
+            return redirect(url_for('auth.login'))
     
     return render_template("login.html")
 
@@ -36,8 +40,8 @@ def signup():
         password2 = request.form.get('password2')
 
         # user = User.query.filter_by(email=email).first()
-        user = User()
-        existing_user = user.find_user(email)
+        
+        existing_user = db.user.find_one(email)
 
         if existing_user:
             flash('Email already exists.', category='error')
@@ -50,18 +54,22 @@ def signup():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            # new_user = [email=email,first_name=first_name,password=password1]
+
             new_user = {
                 "email": email,
                 "username": first_name,
                 "password": password1
             }
+            user = User(email)
             user.create_user(**new_user)
             flash('Account created!', category='success')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
-
     return render_template("signup.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    flash('You are logged out', category='success')
+    return redirect(url_for('auth.login'))
