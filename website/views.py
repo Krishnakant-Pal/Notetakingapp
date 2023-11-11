9b15d13db818e33a3fb4c5013f295527fbb74a8c
“[Krishnakant-pal]” <krishnakant.pal127@gmail.com>
2023-11-11 21:39:58 +0530

    Added notes

--- website/views.py
+++ website/views.py
@@ -6,8 +6,19 @@ from flask_login import login_required,current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/',methods = ["GET", "POST"])
 @login_required
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note)< 1:
+            flash('Note is too short',category='error')
+
+        else:
+            db.notes.insert_one({'note': note, 'user': current_user.email})
+            flash('Note added', category='success')
+            return redirect(url_for('views.home'))
+        
     return render_template('home.html',user = current_user)
 
